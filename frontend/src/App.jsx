ed99092d137453213bdf9c765cd28bdca0ee8568
gnehs <jayuiop@gmail.com>
2023-08-11 11:24:09 +0800

    added userscript install guide

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,31 @@
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <div className="container mt-16">
+        <h1 className="text-4xl font-bold">台灣物流機器人 UserScript</h1>
+        <a
+          href="https://github.com/tasi788/TaiwanDeliveryPlugins/raw/main/userscript/taiwan-delivery.user.js"
+          target="_blank"
+          rel="noreferrer"
+          className="inline-block mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
+        >
+          安裝
+        </a>
+        <details className="mt-4">
+          <summary>沒有跳出安裝畫面？</summary>
+          <ol className="list-decimal list-inside">
+            <li>確認瀏覽器是否有安裝 Tampermonkey</li>
+            <li>
+              <a
+                href="https://chrome.google.com/webstore/detail/gcalenpjmijncebpfijmoaglllgpjagf"
+                className="text-blue-600 underline hover:opacity-75 active:opacity-50"
+              >
+                安裝 Tampermonkey
+              </a>
+            </li>
+          </ol>
+        </details>
+      </div>
     </>
   );
 }
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,23 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ["Lato", "Noto Sans TC", "sans-serif"],
+    },
+    container: {
+      padding: "1rem",
+      center: true,
+      screens: {
+        lg: "768px",
+        xl: "768px",
+        "2xl": "768px",
+      },
+    },
+    extend: {
+      height: {
+        screen: ["100vh" /* fallback for Opera, IE and etc. */, "100svh"],
+      },
+    },
   },
   plugins: [],
 };

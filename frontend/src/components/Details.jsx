90e80c9a592227981db4dda5a02dc7a276b6b5bd
gnehs <jayuiop@gmail.com>
2023-08-11 19:27:18 +0800

    updated Details component

--- frontend/src/components/Details.jsx
+++ frontend/src/components/Details.jsx
@@ -1,10 +1,18 @@
+import { motion, AnimatePresence } from "framer-motion";
+import { useState } from "react";
 export default function Details({ children, title }) {
+  const [isOpen, setIsOpen] = useState(false);
   return (
-    <details className="bg-blue-500 bg-opacity-5 rounded-[.5em] w-full group frosted-glass">
-      <summary className="text-blue-100 font-bold py-2 px-4 rounded cursor-pointer flex items-center justify-between">
+    <div className="bg-blue-500 bg-opacity-5 rounded-[.5em] w-full frosted-glass">
+      <div
+        className="text-blue-100 font-bold py-2 px-4 rounded cursor-pointer flex items-center justify-between"
+        onClick={() => setIsOpen(!isOpen)}
+      >
         <div>{title}</div>
         <svg
-          className="w-4 h-4 transform group-open:rotate-180 transition-transform"
+          className={`w-4 h-4 transform ${
+            isOpen ? "rotate-180" : ""
+          } transition-transform`}
           viewBox="0 0 20 20"
           fill="currentColor"
         >
@@ -14,8 +22,20 @@ export default function Details({ children, title }) {
             d="M10 13.586L3.707 7.293a1 1 0 011.414-1.414L10 10.172l4.879-4.879a1 1 0 111.414 1.414L10 13.586z"
           />
         </svg>
-      </summary>
-      <div className="p-4 pt-0">{children}</div>
-    </details>
+      </div>
+
+      <AnimatePresence mode="wait">
+        {isOpen && (
+          <motion.div
+            initial={{ height: 0, opacity: 0, y: -10 }}
+            animate={{ height: "auto", opacity: 1, y: 0 }}
+            exit={{ height: 0, opacity: 0, y: -10 }}
+            transition={{ duration: 0.2 }}
+          >
+            <div className="p-4 pt-0">{children}</div>
+          </motion.div>
+        )}
+      </AnimatePresence>
+    </div>
   );
 }
